aa9c8662ef4144ec087a33e9a2ce4a99db78ce32
Almaz <mositosi97@gmail.com>
2018-09-19 22:13:52 +0600

    Solve Find a string problem

---
+++ Strings/find_a_string.py
@@ -0,0 +1,14 @@
+from re import findall
+
+def count_substring(string, sub_string):
+    match = findall('(?='+sub_string+')', string)
+    return len(match)
+
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+
+    count = count_substring(string, sub_string)
+    print(count)
+
