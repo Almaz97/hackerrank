c23a227a68eead098a689583959f8b619472c7a8
Almaz <mositosi97@gmail.com>
2018-09-23 22:58:53 +0600

    Solve No Idea problem subdomain:Sets

---
+++ sets/no_idea.py
@@ -0,0 +1,11 @@
+
+
+from collections import defaultdict
+
+n, m = input().split()
+
+sc_ar = input().split()
+
+A = set(input().split())
+B = set(input().split())
+print(sum([(i in A) - (i in B) for i in sc_ar]))
